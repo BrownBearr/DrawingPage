a90888891da0e9811ceb31eb869170f6c336adab
BrownBearr <viswan.shiva@gmail.com>
2022-11-30 13:17:44 -0500

    Got Randomization to work with insatgram

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 fetch('https://api.apify.com/v2/datasets/UlgoOKT00qv1NT5Dw/items?token=apify_api_Qp45jyhhsGpkfFWly7hUIumcbkuvR62bC9du')
 .then(response=>response.json())
 .then(data=>{
-console.log(data[0].displayUrl);
-//document.getElementById("title").innerHTML = ;
-const x = `https://cors-anywhere.herokuapp.com/${data[0].displayUrl}`;
-console.log(x);
-document.getElementById("head").src = x;
+console.log(data[0].url);
+document.getElementById('changeURL').addEventListener('click', () => {
+    console.log('hi');
+    let x = Math.floor(Math.random()*445)
+    const p = data[x].url;
+    const e = '/embed';
+    document.getElementById('embed').src =
+      p + "embed";
+  });
+
 
 
 })
